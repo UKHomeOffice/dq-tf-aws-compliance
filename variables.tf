312558549e10570fbf710f8b08936bedbcc0045b
Jwanderema <wanderema@hotmail.co.uk>
2021-01-15 11:49:46 +0000

    plan

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ENV_ACCT_ID" {
+}
+
+variable "ENV_ACCT_KEY" {
+}
+
+# variable "account_id" {
+#   type = map(string)
+#   default = {
+#     "test" = "797728447925"
+#     # "notprod" = "483846886818"
+#     # "prod"    = "337779336338"
+#   }
+# }
