dad8f1cda531a2ef393d153b14fe118f5a294d1b
Jwanderema <wanderema@hotmail.co.uk>
2021-01-15 12:37:15 +0000

    bucket

--- variables.tf
+++ variables.tf
@@ -11,11 +11,3 @@ variable "namespace" {
 variable "config_name" {
   default = "dq-aws-config"
 }
-
-s3_bucket_name = {
-  dq_aws_config = "s3-dq-log-archive-bucket-${var.NAMESPACE}"
-}
-
-s3_bucket_acl = {
-  dq_aws_config = "private"
-}
