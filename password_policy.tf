7c6b907599254acfeca541f0d525ecf628c99550
JWanderema <wanderema@hotmail.co.uk>
2021-03-08 11:36:21 +0000

    add remaining policies

--- password_policy.tf
+++ password_policy.tf
@@ -1,4 +1,10 @@
 resource "aws_iam_account_password_policy" "dq_password_policy" {
-  provider                = aws.ENV_ACCT
-  minimum_password_length = 14
+  provider                     = aws.ENV_ACCT
+  minimum_password_length      = 14
+  require_lowercase_characters = true
+  require_uppercase_characters = true
+  require_numbers              = true
+  require_symbols              = true
+  max_password_age             = 90
+  password_reuse_prevention    = 5
 }
