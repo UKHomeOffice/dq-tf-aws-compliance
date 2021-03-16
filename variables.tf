10317e50ca7ae0c5833d82365928ab42ba73ccc3
JWanderema <wanderema@hotmail.co.uk>
2021-03-16 14:53:29 +0000

    add lambda

--- variables.tf
+++ variables.tf
@@ -131,10 +131,10 @@ variable "source_identifier" {
     default = "AWSLogs/797728447925/Config/eu-west-2/2021/3/15/ConfigHistory/797728447925_Config_eu-west-2_ConfigHistory_AWS::Config::ResourceCompliance_20210315T163334Z_20210315T163337Z_1.json.gz"
   }
 
-  locals {
-    path_module   = var.path_module != "unset" ? var.path_module : path.module
-  }
-
   variable "path_module" {
     default = "unset"
+  }
+
+  variable "path_modules" {
+    path_module   = var.path_module != "unset" ? var.path_module : path.module
   }
